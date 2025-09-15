2ff1e1688cba27c6a62cb6e9e8a5c88a383de55a
AthenaOS <thuanvatom@gmail.com>
2025-09-15 08:46:45 +0700

    Update vite.config.ts

--- athenaos-frontend/vite.config.ts
+++ athenaos-frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Thêm đoạn code này vào
+  preview: {
+    port: parseInt(process.env.PORT) || 8080, // Dùng port của Cloud Run
+    host: '0.0.0.0' // Chấp nhận kết nối từ mọi địa chỉ IP
+  }
 })
