0ca23b56fb39b9f3ffd9a5b7364968efcef1b088
ThuanDanchoi <ducthuantrandc@gmail.com>
2025-09-25 09:30:52 +1000

    updated files

--- athenaos-frontend/src/main.tsx
+++ athenaos-frontend/src/main.tsx
@@ -5,7 +5,6 @@ import App from './App.tsx';
 import { MantineProvider } from '@mantine/core';
 import '@mantine/carousel/styles.css';
 
-// Import Mantine's CSS
 import '@mantine/core/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
